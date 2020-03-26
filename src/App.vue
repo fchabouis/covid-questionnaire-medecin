d06853c496fa4fa3f88e62ec48efe676ea1a30d0
Francis <fchabouis@gmail.com>
2020-03-26 22:26:31 +0100

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/">Questionnaire</router-link>|
+      <router-link to="/about">À propos</router-link>
     </div>
     <router-view />
   </div>
@@ -15,6 +15,7 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  font-size: 24px;
 }
 
 #nav {
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,80 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div>
+    <div class="question" v-for="(item, i) of history" :key="item.qid">
+      {{ i + 1 }}. {{ form[item.qid].text }}
+      <div class="answers">
+        <span
+          v-for="answer in form[item.qid].answers"
+          :key="answer.text"
+          @click="selectAnswer(i, answer)"
+          :class="[answer.text == item.answer ? 'selectedAnswer' : '', 'answer']"
+        >{{ answer.text }}</span>
+      </div>
+    </div>
+    <div class="result">{{ result }}</div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+const yaml = require("js-yaml");
 
 export default {
   name: "Home",
-  components: {
-    HelloWorld
+  components: {},
+  data() {
+    return {
+      form: {},
+      history: [],
+      result: ""
+    };
+  },
+  methods: {
+    selectAnswer(q, answer) {
+      if (q + 1 !== this.history.length) {
+        this.history = this.history.slice(0, q + 1);
+        this.result = "";
+      }
+      this.$set(this.history[q], "answer", answer.text);
+      if (Number.isInteger(answer.goto)) {
+        this.history.push({ qid: answer.goto });
+      } else {
+        this.result = answer.goto;
+      }
+    }
+  },
+  mounted() {
+    fetch("/form.yml")
+      .then(response => response.text())
+      .then(data => {
+        this.form = yaml.safeLoad(data);
+        this.history = [{ qid: 1 }];
+      });
   }
 };
 </script>
+
+<style lang="scss" scoped>
+.answer {
+  background-color: lightblue;
+  padding: 12px;
+  margin: 12px;
+  cursor: pointer;
+}
+
+.selectedAnswer {
+  background-color: pink;
+}
+
+.answers {
+  padding-top: 24px;
+}
+
+.question {
+  padding-top: 48px;
+}
+
+.result {
+  padding-top: 48px;
+  font-weight: bold;
+}
+</style>
